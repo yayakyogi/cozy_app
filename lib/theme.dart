a2a79d4f3913f1776a143789e9b3c521aeef0393
yayakyogi <yayaktaka@gmail.com>
2022-01-08 09:05:53 +0700

    fix style and color

---
+++ lib/theme.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+const orangeColor = Color(0xffFF9376);
+const purpleColor = Color(0xff5843BE);
+const greyColor = Color(0xff7A7E86);
+const blackColor = Color(0xff000000);
+const whiteColor = Color(0xffFFFFFF);
+
+const TextStyle fw_300 = TextStyle(fontWeight: FontWeight.w300);
+const TextStyle fw_400 = TextStyle(fontWeight: FontWeight.w400);
+const TextStyle fw_500 = TextStyle(fontWeight: FontWeight.w500);
