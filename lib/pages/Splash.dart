929ac18809e237686b903fb07cd2419f0e35b0c3
yayakyogi <yayaktaka@gmail.com>
2022-01-08 09:25:01 +0700

    add safeArea

--- lib/pages/Splash.dart
+++ lib/pages/Splash.dart
@@ -7,59 +7,62 @@ class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 289,
-            width: double.infinity,
-            color: orangeColor,
+        body: SafeArea(
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 289,
+              width: double.infinity,
+              color: orangeColor,
+            ),
           ),
-        ),
-        Align(
-          alignment: Alignment.bottomRight,
-          child: Image.asset(
-            'assets/images/house.png',
-            height: 433,
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Image.asset(
+              'assets/images/house.png',
+              height: 433,
+            ),
           ),
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 60, horizontal: 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.asset(
-                'assets/images/logo.png',
-                width: 50,
-              ),
-              const SizedBox(height: 30),
-              Text(
-                'Find Cozy House \nto Stay and Happy',
-                style: fw_500.copyWith(color: blackColor, fontSize: 24),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                'Stop membuang banyak waktu \npada tempat yang tidak habitable',
-                style: fw_300.copyWith(color: greyColor, fontSize: 16),
-              ),
-              const SizedBox(height: 40),
-              Container(
-                width: 210,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                decoration: BoxDecoration(
-                    color: purpleColor,
-                    borderRadius: BorderRadius.circular(17)),
-                child: Text(
-                  'Explore Now',
-                  style: fw_500.copyWith(color: whiteColor, fontSize: 18),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'assets/images/logo.png',
+                  width: 50,
                 ),
-              ),
-            ],
-          ),
-        )
-      ],
+                const SizedBox(height: 30),
+                Text(
+                  'Find Cozy House \nto Stay and Happy',
+                  style: fw_500.copyWith(color: blackColor, fontSize: 24),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'Stop membuang banyak waktu \npada tempat yang tidak habitable',
+                  style: fw_300.copyWith(color: greyColor, fontSize: 16),
+                ),
+                const SizedBox(height: 40),
+                Container(
+                  width: 210,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: purpleColor,
+                      borderRadius: BorderRadius.circular(17)),
+                  child: Center(
+                    child: Text(
+                      'Explore Now',
+                      style: fw_500.copyWith(color: whiteColor, fontSize: 18),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     ));
   }
 }
