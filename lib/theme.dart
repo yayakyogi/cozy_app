528baaa87bda30d0a8b4c7e8f61e921685e381da
yayakyogi <yayaktaka@gmail.com>
2022-01-08 11:34:48 +0700

    add graycolor

--- lib/theme.dart
+++ lib/theme.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 const orangeColor = Color(0xffFF9376);
 const purpleColor = Color(0xff5843BE);
 const greyColor = Color(0xff7A7E86);
+const grayColor = Color(0xffF6F7F8);
 const blackColor = Color(0xff000000);
 const whiteColor = Color(0xffFFFFFF);
 
