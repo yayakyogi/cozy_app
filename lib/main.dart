8506200ba04ae17c3baa4e8313d3963e0f15a410
yayakyogi <yayaktaka@gmail.com>
2022-01-13 22:48:31 +0700

    fix main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:cozy_app/pages/404.dart';
+import 'package:cozy_app/pages/Detail.dart';
 import 'package:cozy_app/pages/Home.dart';
 import 'package:cozy_app/pages/Splash.dart';
+import 'package:cozy_app/provider/recommended_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +16,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(fontFamily: 'Poppins'),
-        home: const Home());
+    return ChangeNotifierProvider(
+      create: (context) => RecommendedProvider(),
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(fontFamily: 'Poppins'),
+          home: const Splash()),
+    );
   }
 }
